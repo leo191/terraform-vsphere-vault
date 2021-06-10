03f447d1c7d1fa819e1918c689ef6b471c4030df
Subhadip <sb00742266@TechMahindra.com>
2021-06-10 22:31:02 +0530

    vsphere provider added

--- main.tf
+++ main.tf
@@ -1,32 +1,10 @@
-provider infoblox {
-  wapi_version = "2.5"
-}
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
 
-resource infoblox_ip_allocation "this" {
-  network_view_name = var.network_view_name
-  vm_name = var.hostname
-  cidr = var.cidr
-  tenant_id = var.infoblox_tenant_id
-}
-
-resource infoblox_ip_association "this"{
-  network_view_name = var.network_view_name
-  vm_name = vsphere_virtual_machine.this.name
-  cidr = var.cidr
-  mac_addr = vsphere_virtual_machine.this.network_interface.0.mac_address
-  ip_addr = infoblox_ip_allocation.this.ip_addr
-  vm_id = vsphere_virtual_machine.this.id
-  tenant_id = var.infoblox_tenant_id
-}
-
-resource "infoblox_a_record" "this"{
-  network_view_name = var.network_view_name
-  vm_name = vsphere_virtual_machine.this.name
-  cidr = var.cidr
-  ip_addr = infoblox_ip_allocation.this.ip_addr
-  dns_view = var.dns_view
-  zone = var.dns_suffix
-  tenant_id = var.infoblox_tenant_id
+  # If you have a self-signed cert
+  allow_unverified_ssl = false
 }
 
 resource vsphere_virtual_machine "this" {
